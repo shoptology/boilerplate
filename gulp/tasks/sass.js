e0954a53067fd648bef3edaaacc83a26b39f6486
ivanmayes <ivanmayes@gmail.com>
2014-07-16 09:16:23 -0500

    Using gulp-sass instead of gulp-ruby-sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     filter = require('gulp-filter'),
     browserSync = require('browser-sync');
 
@@ -7,10 +7,8 @@ gulp.task('sass', function () {
     gulp.src([
         './public/css/styles.scss'
     ])
-    .pipe(sass({
-        sourcemap: true
-    }))
-    .pipe(gulp.dest('./public/css'))// Write the CSS & Source maps
-    .pipe(filter('**/*.css')) // Filtering stream to only css files
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(sass())
+    .pipe(gulp.dest('./public/css'));// Write the CSS & Source maps
+    //.pipe(filter('**/*.css')) // Filtering stream to only css files
+    //.pipe(browserSync.reload({stream:true}));
 });
