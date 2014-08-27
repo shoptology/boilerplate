debb006d6de24837fe621f1cc9e5987f486091a9
ivanmayes <ivanmayes@gmail.com>
2014-08-27 16:16:17 -0500

    Added brand styleguide into /styleguide, added some watches and tasks to make it easy to build

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -8,8 +8,9 @@ gulp.task('watch', ['setWatch', 'browserSync'], function() {
     var lr = livereload();
 	gulp.watch(['app/public/sass/**/*.scss', '!app/public/sass/styles.scss'], ['processSCSS']);
     gulp.watch('app/patterns/**/*.scss', ['processSCSS']);
-    gulp.watch('app/patterns/**/*.hbs', ['processPatterns', 'server']);
+    gulp.watch(['app/patterns/**/*.hbs', 'styleguide/source/images/**/*'], ['processPatterns', 'server']);
     gulp.watch(['./app.js', './app/**/*.js', './gulp/**/*.js'], ['server']);
+    gulp.watch('styleguide/source/branding/**/*.hbs', ['brandingSite']);
 
 	/*gulp.watch('src/images/**', ['images']);
 	gulp.watch('src/htdocs/**', ['copy']);*/
