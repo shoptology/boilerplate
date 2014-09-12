9cb409ecf3e9551d2b10880dfa561a1ee269f7a3
Michael May <michael.may@goshoptology.com>
2014-09-12 14:43:05 -0500

    Added uglify to browserify workflow;

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -12,6 +12,7 @@ var path         = require('path');
 var gulp         = require('gulp');
 var rename       = require('gulp-rename');
 var browserify   = require('gulp-browserify');
+var uglify       = require('gulp-uglify');
 var watchify     = require('watchify');
 var bundleLogger = require('../util/bundleLogger');
 var handleErrors = require('../util/handleErrors');
@@ -23,6 +24,7 @@ gulp.task('browserify', function() {
             paths : [path.join(process.cwd(), 'node_modules'), path.join(process.cwd(), './app/patterns'), path.join(process.cwd(), './app/lib')],
             extensions : ['.js']
         }))
+        .pipe(uglify())
         .pipe(rename('bundle.js'))
         .pipe(gulp.dest('./app/public/js'));
 });
